a865f760b3f46e1d6309d0d4857e02ec0370a505
fir0j <firoj.is.availble@gmail.com>
2019-09-21 14:11:08 +0530

    improved counter stability

--- js/script.js
+++ js/script.js
@@ -2,99 +2,93 @@
 
 // init Isotope
 var $grid = $('.grid').isotope({
-  itemSelector: '.element-item',
-  layoutMode: 'fitRows',
-  getSortData: {
-    weight: function( itemElem ) {
-      var weight = $( itemElem ).find('.weight').text();
-      return parseFloat( weight.replace( /[\(\)]/g, '') );
-    }
-  }
+	itemSelector: '.element-item',
+	layoutMode: 'fitRows',
+	getSortData: {
+		weight: function(itemElem) {
+			var weight = $(itemElem).find('.weight').text();
+			return parseFloat(weight.replace(/[\(\)]/g, ''));
+		}
+	}
 });
 
 // filter functions
 var filterFns = {
-  // show if number is greater than 50
-  numberGreaterThan50: function() {
-    var number = $(this).find('.number').text();
-    return parseInt( number, 10 ) > 50;
-  }
-
+	// show if number is greater than 50
+	numberGreaterThan50: function() {
+		var number = $(this).find('.number').text();
+		return parseInt(number, 10) > 50;
+	}
 };
 
 // bind filter button click
-$('#filters').on( 'click', 'button', function() {
-  var filterValue = $( this ).attr('data-filter');
-  // use filterFn if matches value
-  filterValue = filterFns[ filterValue ] || filterValue;
-  $grid.isotope({ filter: filterValue });
+$('#filters').on('click', 'button', function() {
+	var filterValue = $(this).attr('data-filter');
+	// use filterFn if matches value
+	filterValue = filterFns[filterValue] || filterValue;
+	$grid.isotope({ filter: filterValue });
 });
 
-
 // change is-checked class on buttons
-$('.button-group').each( function( i, buttonGroup ) {
-  var $buttonGroup = $( buttonGroup );
-  $buttonGroup.on( 'click', 'button', function() {
-    $buttonGroup.find('.is-checked').removeClass('is-checked');
-    $( this ).addClass('is-checked');
-  });
+$('.button-group').each(function(i, buttonGroup) {
+	var $buttonGroup = $(buttonGroup);
+	$buttonGroup.on('click', 'button', function() {
+		$buttonGroup.find('.is-checked').removeClass('is-checked');
+		$(this).addClass('is-checked');
+	});
 });
 
 //owl carousel plugin
 
 $('.owl-carousel').owlCarousel({
-    loop:false,
-    items:10,
-    responsive:{
-        0:{
-            items:1
-        },
-        480:{
-            items:3
-        },
-        968:{
-            items:4
-        }
-    }
-})
+	loop: false,
+	items: 10,
+	responsive: {
+		0: {
+			items: 1
+		},
+		480: {
+			items: 3
+		},
+		968: {
+			items: 4
+		}
+	}
+});
 
+var skillsTopOffset = $('.technicalSkills').offset().top;
 
-var skillsTopOffset = $(".technicalSkills").offset().top;
-
-$(window).scroll(function(){
-  var windowHeight = $(window).height();
-  if(window.pageYOffset > skillsTopOffset-$(window).height()+200){
-    // jquery easypiechart plugin
-    $('.chart').easyPieChart({
-                //your options goes here
-                easing: 'easeInOut',
-                barColor: '#00344e',
-                // trackColor: '#EE5A24',
-                scaleColor: false,
-                lineWidth: 4,
-                size: 152,
-                onStep: function(from, to, percent) {
-                  $(this.el).find('.percent').text(Math.round(percent));
-                }
-            });
-  }
-
+$(window).scroll(function() {
+	var windowHeight = $(window).height();
+	if (window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
+		// jquery easypiechart plugin
+		$('.chart').easyPieChart({
+			//your options goes here
+			easing: 'easeInOut',
+			barColor: '#00344e',
+			// trackColor: '#EE5A24',
+			scaleColor: false,
+			lineWidth: 4,
+			size: 152,
+			onStep: function(from, to, percent) {
+				$(this.el).find('.percent').text(Math.round(percent));
+			}
+		});
+	}
 });
 
 // jquery.countup.js and jquery.waypoints.js
 
 jQuery(document).ready(function($) {
-            $('.counter').counterUp({
-                delay: 10,
-                time: 1000
-            });
-        });
+	$('.counter').counterUp({
+		delay: 1,
+		time: 80
+	});
+});
 
-
-        const element = document.querySelector(".js-tilt");
+const element = document.querySelector('.js-tilt');
 VanillaTilt.init(element);
-element.addEventListener("tiltChange", callback);
-
+element.addEventListener('tiltChange', callback);
 
 // superslides
 
